718a095dd7d8daba3b3a4cedb96bf5d07320f7db
KyawZinLin3 <kyawzinlin14future@gmail.com>
2024-02-15 16:34:41 +0630

    learning

--- 03-reviews/starter/src/App.jsx
+++ 03-reviews/starter/src/App.jsx
@@ -3,12 +3,26 @@ import { useState } from "react";
 import data from "./data";
 
 const App = () => {
-  console.log(data);
-  const [people, setPeople] = useState();
+  const [people, setPeople] = useState(data);
+  console.log(people);
 
   return (
     <main>
-      <article className="review"></article>
+      <article className="review">
+        {people.map((person) => {
+          const { id, name, job, image, text, author } = person;
+          return (
+            <>
+              <div className="img-container">
+                <img src={image} alt={name} className="person-img" />
+              </div>
+              <h4 className="author">{name}</h4>
+              <p className="job">{job}</p>
+              <p className="info">{text}</p>
+            </>
+          );
+        })}
+      </article>
     </main>
   );
 };

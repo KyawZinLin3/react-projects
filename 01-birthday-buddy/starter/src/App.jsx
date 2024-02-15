1c7e133c3fd6baeea8bd857097fe4b116e29e905
KyawZinLin3 <kyawzinlin14future@gmail.com>
2024-02-15 15:16:23 +0630

    learning

--- 01-birthday-buddy/starter/src/App.jsx
+++ 01-birthday-buddy/starter/src/App.jsx
@@ -1,4 +1,40 @@
+import people from "./data";
+import { useState } from "react";
+
 const App = () => {
-  return <h2>Birthday Reminder - Starter</h2>;
+  const [persons, setPersons] = useState(people);
+  // setPersons(people);
+  console.log(persons);
+  const count = persons.length;
+  console.log(count);
+  return (
+    <main>
+      <section className="container">
+        <h3>{count} Birthdays Today</h3>
+        <section>
+          {persons.map((person) => {
+            const { id, name, age, image } = person;
+
+            return (
+              <article className="person" key={id}>
+                <img src={image} alt={name} className="img" />
+                <div>
+                  <h4>{name}</h4>
+                  <p>{age} years</p>
+                </div>
+              </article>
+            );
+          })}
+        </section>
+        <button
+          type="button"
+          className="btn btn-block"
+          onClick={() => setPersons([])}
+        >
+          Clear All
+        </button>
+      </section>
+    </main>
+  );
 };
 export default App;
